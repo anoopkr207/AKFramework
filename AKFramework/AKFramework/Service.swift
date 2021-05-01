ab95d1e6e9c707cc46d87754567e071b8d546f03
Anoop Kumar <anoop.kumar@unthinkable.co>
2021-05-01 15:00:17 +0530

    Custom framework created

---
+++ AKFramework/AKFramework/Service.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func displayInfo() -> String {
+        return "It is a framework created by Anoop Kumar for testing purpose."
+    }
+}
